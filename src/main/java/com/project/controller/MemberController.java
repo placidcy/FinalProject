92af91c9e6ef546e06618e9b2e3f6b7d8897069f
jihyun-Park90 <pjh063099@gmail.com>
2024-09-30 16:39:40 +0900

    memberDAO insert, update 함수 추가

--- src/main/java/com/project/controller/MemberController.java
+++ src/main/java/com/project/controller/MemberController.java
@@ -2,15 +2,25 @@ package com.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.project.model.MemberSO;
 
 
 @Controller
 public class MemberController {
 	
+	MemberSO memberSO;
+	
 	@GetMapping("/login")
 	public String loginHandler() {
 		return "login";
 	}
+	
+//	@PostMapping
+//	public String loginProcessHandler() {
+//		
+//	}
 	
 	@GetMapping("/mypage")
 	public String mypageHandler() {
--- src/main/java/com/project/model/MemberDAO.java
+++ src/main/java/com/project/model/MemberDAO.java
@@ -1,5 +1,48 @@
 package com.project.model;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class MemberDAO {
 
+	private JdbcTemplate jdbcTemplate;
+	private String sql;
+	
+	public MemberDAO(DataSource ds) {
+		this.jdbcTemplate = new JdbcTemplate(ds);
+	}
+	
+	public int inserMember(MemberDO member) {
+		this.sql = "insert into final_member (member_id, m_name, m_acctid, m_acctpwd, m_email, m_tel, m_dept, m_pfp, m_status, m_role) "
+				+ "values (seq_member_id.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		return this.jdbcTemplate.update(sql, member.getM_acctid(), member.getM_acctpwd(), member.getM_email(), member.getM_tel(), member.getM_dept(), member.getM_pfp(), member.getM_status(), member.getM_role());
+	}
+	
+	public int updateEmail(MemberDO member) {
+		this.sql = "update final_member"
+				+ "set m_email=?"
+				+ "where member_id=?";
+		return this.jdbcTemplate.update(sql, member.getM_email(), member.getMember_id());
+	}
+	
+	public int updatePassword(MemberDO member) {
+		this.sql = "update final_member"
+				+ "set m_acctpw=?"
+				+ "where member_id=?";
+		return this.jdbcTemplate.update(sql, member.getM_acctpwd(), member.getMember_id());
+	}
+	
+	public int updateMemberStatus(MemberDO member) {
+		this.sql = "update final_member"
+				+ "set m_status=?"
+				+ "where member_id=?";
+		return this.jdbcTemplate.update(sql, member.getM_status(), member.getMember_id());
+	}
+	
+	public int deleteMemberInfo(MemberDO member) {
+		this.sql = "delete from final_member"
+				+ "where member_id=?";
+		return this.jdbcTemplate.update(sql, member.getMember_id());
+	}
 }
