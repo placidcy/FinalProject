0e245ae507b88de6a0373cb0f37b4c14f6290473
ches409 <ches409@naver.com>
2024-09-30 11:04:04 +0900

    화면구현: PostDO, AttendanceDO 추가

---
+++ src/main/java/com/project/model/PostDO.java
@@ -0,0 +1,74 @@
+package com.project.model;
+
+public class PostDO {
+	
+	private int post_id;
+	private int type_id;
+	private int member_id;
+	private int course_id;
+	private String p_title;
+	private String p_contents;
+	private String p_regdate;
+	private int p_target;
+	private int p_status;
+	
+	public PostDO() {		
+	}
+	
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getType_id() {
+		return type_id;
+	}
+	public void setType_id(int type_id) {
+		this.type_id = type_id;
+	}
+	public int getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(int member_id) {
+		this.member_id = member_id;
+	}
+	public int getCourse_id() {
+		return course_id;
+	}
+	public void setCourse_id(int course_id) {
+		this.course_id = course_id;
+	}
+	public String getP_title() {
+		return p_title;
+	}
+	public void setP_title(String p_title) {
+		this.p_title = p_title;
+	}
+	public String getP_contents() {
+		return p_contents;
+	}
+	public void setP_contents(String p_contents) {
+		this.p_contents = p_contents;
+	}
+	public String getP_regdate() {
+		return p_regdate;
+	}
+	public void setP_regdate(String p_regdate) {
+		this.p_regdate = p_regdate;
+	}
+	public int getP_target() {
+		return p_target;
+	}
+	public void setP_target(int p_target) {
+		this.p_target = p_target;
+	}
+	public int getP_status() {
+		return p_status;
+	}
+	public void setP_status(int p_status) {
+		this.p_status = p_status;
+	}
+	
+	
+}
