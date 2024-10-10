2141b2bfa4d04e3a5bd036d3caff2a91daa9c04e
karist123 <83295412+karist123@users.noreply.github.com>
2024-10-10 17:02:34 +0900

    강의 게시판 공지사항 리스트

--- src/main/java/com/project/controller/CourseController.java
+++ src/main/java/com/project/controller/CourseController.java
@@ -1,35 +1,54 @@
 package com.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.project.model.CourseNoticeItem;
+import com.project.model.dao.CourseNoticeDAO;
 
 @Controller
 public class CourseController {
+	@Autowired
+	private CourseNoticeDAO courseNoticeDAO;
 
 	@GetMapping("/home")
 	public String course_homeHandler() {
 		return "course_home";
 	}
-	
+
 	@GetMapping("/alert")
 	public String alertHandler() {
 		return "alert";
 	}
-	
+
 	@GetMapping("acceptanceManagement")
 	public String acceptance_managementHandler() {
 		return "acceptance_management";
 	}
-	
+
 	@GetMapping("calendarForm")
 	public String calendarFormHandler() {
 		return "calendarForm";
 	}
-	
+
 	@GetMapping("courseAttend")
 	public String courseAttendHandler() {
 		return "course_attend";
 	}
-	
-	
+
+	@GetMapping("/courseNotice")
+	public String getCourseNotices(Model model,
+			@RequestParam(required = false, defaultValue = "1", name = "page") String page) {
+		int pageNum = Integer.parseInt(page);
+		model.addAttribute("courseAnnouncements", courseNoticeDAO.selectAll(pageNum, 10)); // 첫 페이지의 공지사항 10개 가져오기
+		model.addAttribute("size", courseNoticeDAO.getCount());
+		model.addAttribute("page", pageNum);
+		model.addAttribute("menu", "courseNotice");
+		return "course_notice"; // JSP 파일 이름
+	}
 }
---
+++ src/main/java/com/project/model/CourseNoticeItem.java
@@ -0,0 +1,57 @@
+package com.project.model;
+
+public class CourseNoticeItem extends CourseNoticePostItem {
+    private int target;
+
+    public CourseNoticeItem() {
+        super();
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public int getNoticeId() {
+        return super.getPostId();
+    }
+
+    public void setNoticeId(int noticeId) {
+        super.setPostId(noticeId);
+    }
+
+    public String getNoticeTitle() {
+        return super.getNoticeTitle();
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        super.setNoticeTitle(noticeTitle);
+    }
+
+    public String getNoticeContents() {
+        return super.getNoticeContents();
+    }
+
+    public void setNoticeContents(String noticeContents) {
+        super.setNoticeContents(noticeContents);
+    }
+    
+    public String getRegDate() {
+    	return super.getRegDate();
+    }
+    
+    public void setRegDate(String regdate) {
+    	super.setRegDate(regdate);
+    }
+    
+    public String getAttachment() {
+        return super.getAttachment();
+    }
+
+    public void setAttachment(String attachment) {
+        super.setAttachment(attachment);
+    }
+}
