98890fe6875f8a27d93c2b644f6e95d8af31a601
junmipark <junmipark1@gmail.com>
2024-10-18 10:44:01 +0900

    기타: 공지사항 관련 샘플 데이터 삽입문 추가

---
+++ src/main/resources/sql/notice.sql
@@ -0,0 +1,17 @@
+-- 공지사항 관련 샘플 데이터 삽입
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'일부 QR 코드 인식 장애 안내(조치완료)','일부 QR 코드 인식 장애 안내(조치완료)',to_date('2024-06-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'입실 시간 변경 안내','',to_date('2024-06-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 점검 안내(2024.07.22(월) 14:30 ~ 24:00','',to_date('2024-07-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 접속 오류 안내(조치 완료)','',to_date('2024-07-22', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'근로자의 날(5월 1일) 휴무 안내','',to_date('2024-04-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'일반 로그인 장애 안내(조치 완료)','',to_date('2024-08-01', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 점검 안내(2024.08.12(월) 19:00 ~ 24:00)','',to_date('2024-08-05', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'2024년 1분기 오프라인 강의 안내','',to_date('2024-03-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'일부 QR 코드 인식 장애 안내(조치완료)','일부 QR 코드 인식 장애 안내(조치완료)',to_date('2024-06-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'입실 시간 변경 안내','',to_date('2023-06-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 점검 안내(2023.07.22(월) 14:30 ~ 24:00','',to_date('2023-07-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 접속 오류 안내(조치 완료)','',to_date('2023-07-22', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'근로자의 날(5월 1일) 휴무 안내','',to_date('2023-04-20', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'일반 로그인 장애 안내(조치 완료)','',to_date('2023-08-01', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'홈페이지 점검 안내(2023.08.12(월) 19:00 ~ 24:00)','',to_date('2023-08-05', 'yyyy-mm-dd'),0,DEFAULT);
+INSERT INTO FINAL_COURSE_POST fcp VALUES(seq_post_id.nextval,0,1,NULL,'2023년 1분기 오프라인 강의 안내','',to_date('2023-03-20', 'yyyy-mm-dd'),0,DEFAULT);
