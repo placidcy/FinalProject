ae29bae59fe8d57bd980a2c0c0abb9c6fda07e65
ches409 <ches409@naver.com>
2024-10-18 16:43:53 +0900

    기타:https관련 수정

--- src/main/java/com/project/config/MvcConfig.java
+++ src/main/java/com/project/config/MvcConfig.java
@@ -26,7 +26,7 @@ public class MvcConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://13.238.185.11:3000")
 		.allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
 		.allowedHeaders("*")
 		.allowCredentials(true)
