60a0f982783dd4bab0f3bec08c92cbf8ba8d1262
junmipark <junmipark1@gmail.com>
2024-10-18 09:31:21 +0900

    Revert "Merge pull request #84 from ches409/pjm"
    This reverts commit 37821daf7b26239ed37f289eecd2bf3b43e14249, reversing
    changes made to 19776b3216f7e2b2727adccdbfb44d7b3572baec.

--- src/main/java/com/project/service/ImageUploadSO.java
+++ src/main/java/com/project/service/ImageUploadSO.java
@@ -13,9 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
 public class ImageUploadSO {
@@ -27,30 +25,12 @@ public class ImageUploadSO {
 
 	public ResponseEntity<String> uploadFile(MultipartFile file) throws SdkClientException, IOException {
 		String fileName, fileUrl;
-		ObjectMetadata metadata;
-		PutObjectRequest request;
 		try {
-			/* 업로드 파일 원본 이름 가져오기 */
 			fileName = file.getOriginalFilename();
-
-			/* 파일 메타데이터 설정 */
-			metadata = new ObjectMetadata();
-			metadata.setContentType(file.getContentType());
-			metadata.setContentLength(file.getSize());
-
-			/* 업로드 요청 생성 및 읽기 권한 부여 */
-			request = new PutObjectRequest(bucketName, fileName, file.getInputStream(), metadata)
-					.withCannedAcl(CannedAccessControlList.PublicRead);
-
-			/* 파일 업로드 */
-			amazonS3.putObject(request);
-
-			/* 업로드 URL 반환 */
+			amazonS3.putObject(bucketName, fileName, file.getInputStream(), new ObjectMetadata());
 			fileUrl = "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
-
 			return ResponseEntity.ok(fileUrl);
 		} catch (AmazonServiceException e) {
-			/* 업로드 실패 시, 오류 메시지 반환 */
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("File upload failed: " + e.getMessage());
 		}
