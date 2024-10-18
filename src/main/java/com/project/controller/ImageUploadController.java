9d30c57666b6c659afdff294187844c1996adc52
junmipark <junmipark1@gmail.com>
2024-10-18 11:17:27 +0900

    기타: 이미지 업로드 구현 코드 복구
    merge 과정에서 소실된 소스코드 복구

---
+++ src/main/java/com/project/controller/ImageUploadController.java
@@ -0,0 +1,24 @@
+package com.project.controller;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import com.amazonaws.SdkClientException;
+import com.project.service.*;
+@Controller
+public class ImageUploadController {
+	@Autowired
+	ImageUploadSO uploadSO;
+	@RequestMapping("/upload/testPage")
+	public String test() {
+		return "main/test";
+	}
+	@RequestMapping("/upload")
+	public ResponseEntity<String> uploadFile(@RequestParam(name = "file", required = true) MultipartFile file)
+			throws SdkClientException, IOException {
+		return uploadSO.uploadFile(file);
+	}
+}
