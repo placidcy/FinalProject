3ac1c4efd03835d6967d6e86f3ef3ec10f2b6403
karist123 <kshlogosk01@gmail.com>
2024-10-22 18:45:45 +0900

    app, index 수정

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CourseBoard from './course_board';
 import CourseMaterials from './course_meterials';
 import WriteMaterials from './write_materials';
 
+
 const App = () => {
 	return (
-		<Routes>
-			<Route path="/CourseBoard" element={<CourseBoard />} />
-			<Route path="/CourseMaterials" element={<CourseMaterials />} />
-			<Route path="/WriteMaterials" element={<WriteMaterials />} />
-		</Routes>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/CourseBoard" element={<CourseBoard />} />
+				<Route path="/CourseMaterials" element={<CourseMaterials />} />
+				<Route path="/WriteMaterials" element={<WriteMaterials />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
